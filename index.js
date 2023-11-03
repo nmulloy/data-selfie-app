954083e857ddd855001100a7a522a9035abcf4e0
nmulloy <nmulloy@tcd.ie>
2023-11-03 21:03:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const Datastore = require('nedb')
+
+const app = express();
+app.listen(3000, () => console.log("listening at 3000")); 
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'} ));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+
+app.get('/api', (request, response) =>{
+    response.json({ test: 123});
+})
+
+app.post('/api', (request, response) => {
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data)
+    response.json(data)
+});
