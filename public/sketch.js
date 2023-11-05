5b23323a6a0f1530dc60633be040009b6ff5ea48
nmulloy <nmulloy@tcd.ie>
2023-11-05 19:02:54 +0000

    finished project

---
+++ public/sketch.js
@@ -0,0 +1,43 @@
+
+function setup(){
+            
+    noCanvas();
+    const video = createCapture(VIDEO)
+    video.size(320,240)
+
+
+    let lon, lat;
+    const button = document.getElementById('submit');
+    button.addEventListener('click', async event => {
+    const mood = document.getElementById('mood').value;
+    video.loadPixels();
+    const image64 = video.canvas.toDataURL()
+    const data = {lat, lon, mood, image64}
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+     };
+    const response = await fetch('/api', options);
+    const json = await response.json();
+    console.log(json)
+    });
+
+if('geolocation' in navigator){
+    console.log('geolocation available');
+    navigator.geolocation.getCurrentPosition(position => {
+             lat = position.coords.latitude;
+             lon = position.coords.longitude;
+            document.getElementById('latitude').textContent = lat;
+            document.getElementById('longitude').textContent = lon;
+            //console.log(position)
+            })
+    } else{
+    console.log('geolocation is not available')
+}
+
+}
+
+
